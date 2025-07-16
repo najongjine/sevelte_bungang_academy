508f76e8d86a7552d0e0a58eced7d427861903ce
najongjine <najongjin3@hotmail.com>
2025-07-16 10:10:04 +0900

    .

--- src/lib/stores/userStoreType.ts
+++ src/lib/stores/userStoreType.ts
@@ -1,10 +1,18 @@
+export interface UserRole {
+	role_idp: string;
+	user_idp: string;
+	irole: string;
+	created_dt: string; // 또는 Date 타입을 원하면 Date
+}
+
 export interface UserType {
-	idp: number;
-	uid?: string;
-	email?: number;
-	displayname?: string;
-	photourl?: string;
-	providerid?: string;
-	created_dt?: string;
-	updated_dt?: string;
+	idp: string;
+	uid: string;
+	email: string;
+	displayname: string;
+	photourl: string;
+	providerid: string;
+	created_dt: string;
+	updated_dt: string;
+	roles: UserRole[]; // JSON 배열 형태로 들어옴
 }
