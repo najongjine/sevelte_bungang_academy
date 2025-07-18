e60225bb7d58298ed7ea9c71add4004ddf3518af
najongjine <najongjin3@hotmail.com>
2025-07-18 11:31:21 +0900

    .

--- src/routes/product_upload/+page.ts
+++ src/routes/product_upload/+page.ts
@@ -1,10 +1,11 @@
-// +page.ts
-export function load({ url }) {
-	const product_idp = Number(url.searchParams.get('product_idp') ?? 0);
-	const category_idp = Number(url.searchParams.get('category_idp') ?? 0);
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = ({ url }) => {
+	const product_idp = Number(url.searchParams.get('product_idp'));
+	const category_idp = Number(url.searchParams.get('category_idp'));
 
 	return {
 		product_idp,
 		category_idp
 	};
-}
+};
