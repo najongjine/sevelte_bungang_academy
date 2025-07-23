41536e2bd0d65936776d5d2ea8238126198df73f
najongjine <najongjin3@hotmail.com>
2025-07-23 11:43:30 +0900

    .

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,13 +3,28 @@
 </script>
 
 <header>
-	<!-- 헤더 -->
-	<div class="w-full border-b px-4 py-3 flex items-center gap-4">
-		<h1 class="text-xl font-bold text-black">나의장터</h1>
+	<!-- 헤더 (로고 + 검색창 + 로그인) -->
+	<div class="w-full px-4 py-3 flex items-center gap-4">
+		<h1 class="text-xl font-bold text-purple-700">
+			<a href="/">나의장터</a>
+		</h1>
 		<input
 			class="flex-1 border px-4 py-2 rounded text-sm"
 			placeholder="상품명, 지역명, @상점명 입력"
 		/>
 		<button class="border px-4 py-2 text-sm">로그인</button>
 	</div>
+
+	<!-- 상품 올리기: 왼쪽으로 넓게 들여쓰기 -->
+	<div class="w-full pt-4 text-sm">
+		<a href="/product_upload"
+			><button class="text-black hover:underline" style="margin-left: 7rem;">
+				상품 올리기
+			</button></a
+		>
+		<br /><br />
+	</div>
+
+	<!-- 구분선 -->
+	<hr class="border-t-2 border-gray-400" />
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,15 @@
 	const api = import.meta.env.VITE_SERVER_API_URL;
 
 	let products: ProductDetail[] = [];
+	let page_no = 1;
+	let item_limit = 50;
+	let guide_msg = '로딩중';
 
 	const fetchProductList = async () => {
 		try {
-			let response: any = await axios.get(`${api}/api/product/get_product_list`, {});
+			let response: any = await axios.get(`${api}/api/product/get_product_list`, {
+				params: { page_no, item_limit }
+			});
 			response = response?.data;
 
 			if (!response?.success) {
@@ -18,6 +23,7 @@
 				return;
 			}
 			products = response?.data ?? [];
+			if (!products?.length) guide_msg = `데이터가 없습니다.`;
 		} catch (err: any) {
 			alert(`서버 에러. ${err?.message ?? ''}`);
 		} finally {
@@ -31,5 +37,9 @@
 {#if products?.length}
 	<ProductList {products} />
 {:else}
-	<p>로딩 중...</p>
+	<p>{guide_msg}</p>
 {/if}
+<br /><br />
+<div>
+	<button>데이터 더 가져오기</button>
+</div>
