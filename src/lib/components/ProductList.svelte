8f3139303a3b8be86cffb562a71b08c379614f9e
najongjine <najongjin3@hotmail.com>
2025-07-23 11:02:15 +0900

    .

--- src/lib/components/ProductList.svelte
+++ src/lib/components/ProductList.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
+	import type { ProductDetail } from '$lib/types/product_type';
+
 	/**
 	 * This prop is required.
 	 */
-	export let products = [] as any[];
+	export let products: ProductDetail[]; // 기본값 생략 (선언만)
+	console.log(`## products: `, products);
 </script>
 
 <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
 	{#each products as p}
 		<div class="border rounded shadow-sm hover:shadow-md transition">
-			<img src={p?.img} alt={p?.title} class="w-full h-auto" />
+			<img src={p?.imgs[0].img_url} alt={p?.title} class="w-full h-auto" />
 			<div class="p-2">
 				<h3 class="text-sm font-medium truncate">{p?.title}</h3>
 				<p class="text-red-500 font-bold mt-1">{p?.price}</p>
-				<p class="text-gray-500 text-xs mt-1">{p?.time}</p>
+				<p class="text-gray-500 text-xs mt-1">{p?.created_dt}</p>
 			</div>
 		</div>
 	{/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,36 @@
 <script lang="ts">
 	import Banner from '$lib/components/Banner.svelte';
 	import ProductList from '$lib/components/ProductList.svelte';
+	import type { ProductDetail } from '$lib/types/product_type';
 	import axios from 'axios';
 
-	let products: any[] = [];
+	const api = import.meta.env.VITE_SERVER_API_URL;
+
+	let products: ProductDetail[] = [];
 
 	const fetchProductList = async () => {
 		try {
-			const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
-			products = response?.data;
-			console.log(`## products:`, products);
+			let response: any = await axios.get(`${api}/api/product/get_product_list`, {});
+			response = response?.data;
+
+			if (!response?.success) {
+				alert(`제품정보를 가져오는데 에러가 발생했습니다. ${response?.message ?? ''}`);
+				return;
+			}
+			products = response?.data ?? [];
+			console.log(`## on mount products: `, products);
 		} catch (err: any) {
 			alert(`서버 에러. ${err?.message ?? ''}`);
 		} finally {
 		}
 	};
-
 	fetchProductList();
 </script>
 
 <Banner />
 
-<ProductList {products} />
+{#if products?.length}
+	<ProductList {products} />
+{:else}
+	<p>로딩 중...</p>
+{/if}
