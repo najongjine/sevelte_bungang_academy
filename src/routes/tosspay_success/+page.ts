92a9e9e596a602ff016eb4f3b4b0d825d3ec2a83
najongjine <najongjin3@hotmail.com>
2025-07-25 11:26:01 +0900

    .

--- src/routes/tosspay_success/+page.ts
+++ src/routes/tosspay_success/+page.ts
@@ -1,9 +1,9 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = ({ url }) => {
-	const paymentType = Number(url.searchParams.get('paymentType'));
-	const orderId = Number(url.searchParams.get('orderId'));
-	const paymentKey = Number(url.searchParams.get('paymentKey'));
+	const paymentType = String(url.searchParams.get('paymentType'));
+	const orderId = String(url.searchParams.get('orderId'));
+	const paymentKey = String(url.searchParams.get('paymentKey'));
 	const amount = Number(url.searchParams.get('amount'));
 
 	return {
