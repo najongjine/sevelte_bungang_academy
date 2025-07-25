a0d3cf84b377d4a9fdc09ce6860ee597baa6e163
najongjine <najongjin3@hotmail.com>
2025-07-25 11:26:59 +0900

    .

--- src/routes/tosspay_success/+page.svelte
+++ src/routes/tosspay_success/+page.svelte
@@ -10,6 +10,7 @@
 </script>
 
 <h1>결제 성공</h1>
-<p>결제 키: {data?.paymentKey}</p>
-<p>주문 ID: {data?.orderId}</p>
-<p>결제 금액: {data?.amount}</p>
+<p>paymentType: {paymentType}</p>
+<p>결제 키: {paymentKey}</p>
+<p>주문 ID: {orderId}</p>
+<p>결제 금액: {amount}</p>
